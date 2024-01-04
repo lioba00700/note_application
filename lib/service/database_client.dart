578cdfd021672d1b44be2339be7f8413e912b4aa
lioba00700 <lioba00700@naver.com>
2024-01-04 12:30:23 +0900

    커밋

--- lib/service/database_client.dart
+++ lib/service/database_client.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+
 class DatabaseClient {
   static final DatabaseClient _instance = DatabaseClient();
 
-  void initializeDatabase() {
-    
+  void initializeDatabase() async {
+    Directory dir = await getApplicationCacheDirectory();
   }
 }
