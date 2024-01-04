ee3ef34fb8c8b1e50e2d3cac43141f5923352b70
lioba00700 <lioba00700@naver.com>
2024-01-04 11:19:40 +0900

    커밋

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,13 +1,43 @@
 import "package:flutter/material.dart";
 
-class MainPage extends StatelessWidget{
+class MainPage extends StatelessWidget{//stl
   const MainPage({super.key});
 
   @override
   Widget build(BuildContext context){
-    return const Placeholder()
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('나의 하루'),
+      ),
+      body: Column(
+        children: [
+          CalendarDatePicker(
+            initialDate: DateTime.now(),
+            firstDate: DateTime(1900),
+            lastDate: DateTime(2300),
+            onDateChanged: (value) {},
+          ),
+          Expanded(
+          child: Padding(padding: const EdgeInsets.all(10.0),
+          child: ListView.builder(
+            itemBuilder:(context,index) =>  ListTile(
+            title: Text('$index'),
+            trailing: IconButton(onPressed: null,
+            icon: Icon(Icons.delete),
+              ),
+            ),
+          itemCount: 100,
+          ),
+          )
+          )
+        ],
+      ),
+      floatingActionButton: const FloatingActionButton(onPressed: null,child: Icon(Icons.add),
+      ),
+    );
   }
 }
+
 
 
 
